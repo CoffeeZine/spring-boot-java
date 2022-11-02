3516cde4cb7fb82fa1b722179337097caf9c1fb0
sowai00 <72919700+sowai00@users.noreply.github.com>
2022-11-02 12:05:09 +0800

    spring

---
+++ src/test/java/com/example/SpringWebTesting/SpringWebTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
